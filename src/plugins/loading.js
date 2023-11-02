33420e2ed02153a9fbe0ad22fb99ed96f70f723f
iamjowin <34188137+iamjowin@users.noreply.github.com>
2023-11-02 12:04:53 +0800

    about loading default

--- src/plugins/loading.js
+++ src/plugins/loading.js
@@ -3,6 +3,7 @@ export function handleHideLoading(container, timeout) {
         container.style.display = 'none';
 
         animateLeftColumn();
+        animateAboutSection();
     }, timeout);
 }
 
@@ -10,4 +11,10 @@ function animateLeftColumn() {
     const leftDiv = document.querySelector('.left-column-div');
 
     leftDiv.classList.add('fade-in-left-animation')  
+}
+
+function animateAboutSection() {
+    const aboutSection = document.querySelector('#section1');
+
+    aboutSection.classList.remove('opacity-0')
 }
