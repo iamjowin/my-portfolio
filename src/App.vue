ecd348b280472e6c9e568c723577e1f39ec6453d
iamjowin <34188137+iamjowin@users.noreply.github.com>
2023-11-02 14:18:33 +0800

    update hover shoot

--- src/App.vue
+++ src/App.vue
@@ -285,7 +285,7 @@ onMounted(() => {
                     <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#5be3d4] peer-focus:dark:text-[#5be3d4] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Message</label>
                 </div>
                 
-                <button type="submit" class="flex flex-wrap mt-10 text-[#5be3d4] bg-black hover:bg-blue-800 font-medium text-sm px-5 py-2.5 text-justify">Shoot
+                <button type="submit" class="flex flex-wrap mt-10 text-[#5be3d4] bg-black hover:bg-[#000] font-medium text-sm px-5 py-2.5 text-justify">Shoot
                     <PaperAirplaneIcon class="h-4 w-5 text-[#5be3d4] mt-0.5 ml-1" />
                 </button>
             </form>
